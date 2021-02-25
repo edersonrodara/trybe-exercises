40bb1ea3016f70f6e9ec6142913bede153379bba
edersonrodara <edersonrodara@gmail.com>
2021-02-25 15:37:22 -0300

    dias 1 e 2

---
+++ aulasJS/bloco-4/dia-01/exercicio-01.js
@@ -0,0 +1,47 @@
+// const name = "Ederson Rodrigues Araujo";
+// const birthCity = "Porto-velho - RO";
+// let birthYear = 1986;
+// birthYear = 2020;
+// birthCity = "Fortaleza - CE";
+
+// console.log ("Nome : " + name);
+// console.log ("Nacionalidade : " + birthCity);
+// console.log ("Data de nascimento : " + birthYear);
+//==============================================================
+
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientId);
+// console.log(typeof isEnrolled);
+// console.log(typeof patientInfo);
+// console.log(typeof firstName);
+// console.log(typeof lastName);
+// console.log(typeof patientEmail);
+//==============================================================
+
+// let base = 5;
+// let altura = 8;
+// let area = base * altura;
+// let perimetro = base * 2 + altura * 2;
+
+// console.log (area);
+// console.log (perimetro);
+//==============================================================
+
+let nota = 79;
+
+nota = 59.99
+
+if (nota < 60) {
+    console.log ("Você foi reprovada(o)");
+} else if (nota >= 60 && nota <= 80) {
+    console.log ("Você está na nossa lista de espera");
+} else {
+    console.log ("Parabéns, você foi aprovada(o)!");
+}

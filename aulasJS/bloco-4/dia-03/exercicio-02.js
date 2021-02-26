688e79a1036f9f4273c211d06c153b474b73644f
edersonrodara <edersonrodara@gmail.com>
2021-02-26 16:09:00 -0300

    correção do exercício2

---
+++ aulasJS/bloco-4/dia-03/exercicio-02.js
@@ -0,0 +1,8 @@
+let linhaDeAsteriscos = [];
+let quantidadeDeAsterisco = 5;
+let quantidadeDeLinhas = 5;
+
+for (let indexLinha = 1; indexLinha <= quantidadeDeAsterisco; indexLinha += 1) {
+    linhaDeAsteriscos += "*";
+    console.log (linhaDeAsteriscos);
+  }

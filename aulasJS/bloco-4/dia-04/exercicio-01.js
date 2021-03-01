a61cc886c924b7095a4cca685ebd2c9646c434c7
edersonrodara <edersonrodara@gmail.com>
2021-03-01 16:08:44 -0300

    exercício 1, 2 e 3 concluido

--- aulasJS/bloco-4/dia-04/exercicio-01.js
+++ aulasJS/bloco-4/dia-04/exercicio-01.js
@@ -13,8 +13,21 @@
 // console.log (`A jogadora ${player.name} ${player.name} foi eleita a melhor do mundo por ${player.bestInTheWorld.length} vezes`);
 // console.log (`A jogadora possui ${player.medals.golden} medalhas de ouro e ${player.medals.silver} medalhas de prata`);
 
-function soma(a, b) {
-  return a + b;
-}
+// function soma(a, b) {
+//   return a + b;
+// }
 
-console.log(soma(15, 16));
+// console.log(soma(15, 16));
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  recorrent: "Sim"
+};
+
+console.log(`Bem-vinda, ${info.personagem}`);
+console.log(info);
+
+for (const key in info) {
+  console.log(key);
+}
